2c249b9ca0eac5d4b3de4d352a61a0d9e9099f18
Andrehlb <andreh.72@hotmail.com>
2025-10-03 23:12:57 -0300

    feat: add SplashActivity for application launch screen

---
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/SplashActivity.kt
@@ -0,0 +1,8 @@
+package br.com.venturus.andrehlb.minipokedex
+
+import .android.content.Intent
+import .android.os.Bundle
+import .android.os.Handler
+import .android.os.Looper
+import .androidx.appcompat.app.AppCompatActivity
+
