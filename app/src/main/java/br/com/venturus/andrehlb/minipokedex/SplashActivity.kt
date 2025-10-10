60243a9808659e9a2186c760e7e61d35a2881f1b
Andrehlb <andreh.72@hotmail.com>
2025-10-10 19:42:02 -0300

    fix: correct import statements and class declaration in SplashActivity

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/SplashActivity.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/SplashActivity.kt
@@ -1,11 +1,11 @@
 package br.com.venturus.andrehlb.minipokedex
 
-import .android.content.Intent
-import .android.os.Bundle
-import .android.os.Handler
-import .android.os.Looper
-import .androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 clash splashActivity: AppCompactActivity() {
-
+    override fun onCreate(savedinstanceState: Bundle?)
 }
