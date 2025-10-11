d35f3fabf5e61b668dee0fae52cd55dc74b08322
Andrehlb <andreh.72@hotmail.com>
2025-10-11 11:50:20 -0300

    feat: add lifecycle logging to MainActivity

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/MainActivity.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/MainActivity.kt
@@ -1,20 +1,58 @@
 package br.com.venturus.andrehlb.minipokedex
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate chamado")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart chamado")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume chamado")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause chamado")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop chamado")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop chamado")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy chamado")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart chamado")
     }
 }
