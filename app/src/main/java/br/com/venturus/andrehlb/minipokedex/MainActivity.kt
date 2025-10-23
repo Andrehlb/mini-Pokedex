7b197329244d8ab53c531709a8c16139fc8b95bc
Andrehlb <andreh.72@hotmail.com>
2025-10-23 05:32:52 -0300

    feat: Add DetailActivity and declare it in AndroidManifest
    Adds a new, empty `DetailActivity.kt`. This activity uses view binding with `ActivityDetailBinding` and sets its content view in `onCreate`.
    
    The new activity is also declared in `AndroidManifest.xml` to make it accessible to the application.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/MainActivity.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/MainActivity.kt
@@ -4,22 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.databinding.DataBindingUtil
+// import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import br.com.venturus.andrehlb.minipokedex.databinding.ActivityMainBinding
-
+import br.com.venturus.andrehlb.minipokedex.PokemonListViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: PokemonListViewModel
     private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Installs the splash screen. This must be called before super.onCreate()
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main) // Data Biding
+        biding.lifecycleOwner = this // Para LiveData reagir ao ciclo de vida da Activity
+        viewModel = ViewModelProvider(this)[PokemonListViewModel::class.java] // Instância do ViewModel
+        binding.viewModel = viewModel
+
         Log.d(tag, "onCreate chamado")
         binding.searchEditText.requestFocus()
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
