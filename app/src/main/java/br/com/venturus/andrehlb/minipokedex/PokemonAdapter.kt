8dcc66ec7358abdec3dda993b175808500075bec
Andrehlb <andreh.72@hotmail.com>
2025-10-25 19:41:44 -0300

    feat: Implement initial structure for PokemonAdapter
    Creates the basic structure for the `PokemonAdapter` using `ListAdapter` to efficiently handle data updates in a `RecyclerView`.
    
    Key changes include:
    - Defining a `Pokemon` data class with `id` and `name` properties.
    - Setting up `PokemonAdapter` to extend `ListAdapter` with a `DiffUtil.ItemCallback`.
    - Implementing a `PokemonViewHolder` that binds a `Pokemon` object to the `ItemPokemonBinding`.
    - Binding the Pokémon's name to the `pokemonName` TextView in the item layout.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/PokemonAdapter.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/PokemonAdapter.kt
@@ -1,4 +1,21 @@
 package br.com.venturus.andrehlb.minipokedex
 
-class PokemonAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.venturus.andrehlb.minipokedex.databinding.ItemPokemonBinding
+
+data class Pokemon(val id: Int, val name: String)
+
+class PokemonAdapter : ListAdapter<Pokemon, PokemonAdapter.PokemonViewHolder>(DiffCallback) {
+
+    class PokemonViewHolder(private val binding: ItemPokemonBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(pokemon: Pokemon) {
+            binding.pokemonName.text = pokemon.name
+            // serão adicionados mais bindings aqui futuramente
+        }
+    }
+
 }
