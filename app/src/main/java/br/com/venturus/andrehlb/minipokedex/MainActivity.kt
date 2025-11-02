40af369942fc40076168492e83163c934ef814aa
Andrehlb <andreh.72@hotmail.com>
2025-11-01 21:53:35 -0300

    chore: Remove unused import in MainActivity
    Removes an unused import for `LinearLayoutManager` from `MainActivity.kt`.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/MainActivity.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import android.recyclerview.widgrt.LinearLayoutManager
 import br.com.venturus.andrehlb.minipokedex.databinding.ActivityMainBinding
 import br.com.venturus.andrehlb.minipokedex.viewmodel.PokemonListViewModel
 
