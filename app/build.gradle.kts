59dc24165139d72647fe6feda2c8d01622f0b620
Andrehlb <andreh.72@hotmail.com>
2025-11-02 18:48:29 -0300

    feat: Add Kotlin Coroutines dependency
    Adds the `kotlinx-coroutines-android` dependency to the project. This will enable the use of coroutines for handling asynchronous operations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,8 @@ dependencies {
     // Retrrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
 
