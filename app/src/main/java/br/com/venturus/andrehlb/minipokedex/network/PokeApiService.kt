b9356e80e73b795161e3ac482ecd9a0c46b42495
Andrehlb <andreh.72@hotmail.com>
2025-11-02 19:38:07 -0300

    feat: Add pagination parameters to getPokemonList
    Updates the `getPokemonList` function in the `PokeApiService` interface to support pagination.
    
    This change adds `limit` and `offset` query parameters to the `pokemon` endpoint request, allowing for fetching the Pokémon list in paginated chunks. Default values are set to 20 for `limit` and 0 for `offset`.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/PokeApiService.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/PokeApiService.kt
@@ -2,8 +2,14 @@ package br.com.venturus.andrehlb.minipokedex.network
 
 import br.com.venturus.andrehlb.minipokedex.model.PokemonResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 
 interface PokeApiService {
     @GET("pokemon")
-    suspend fun getPokemonList(): PokemonResponse
+    suspend fun getPokemonList(
+        @Query("limit") limit: Int = 20,
+        @Query("offset") offset: Int = 0
+    ): PokemonResponse
 }
