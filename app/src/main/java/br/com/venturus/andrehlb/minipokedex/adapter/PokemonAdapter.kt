ee4811723af6a4d5952433436ff0cbd23bdd823b
Andrehlb <andreh.72@hotmail.com>
2025-11-02 00:29:52 -0300

    feat: Create Pokemon data class
    Adds a new data class named `Pokemon` to represent a Pokémon's basic data.
    
    Key changes:
    - Creates the `Pokemon.kt` file.
    - Defines the `Pokemon` data class with `id` (Int) and `name` (String) properties.

---
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/adapter/PokemonAdapter.kt
@@ -0,0 +1,17 @@
+package br.com.venturus.andrehlb.minipokedex.adapter
+
+import android.view.LatoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.venturus.andrehlb.minipokedex.databinding.ItemPokemonBinding
+import br.com.venturus.andrehlb.minipokedex.model.Pokemon
+
+class PokemonAdapter : ListAdapter<Pokemon, PokemonAdapter.PokemonViewHolder>(PokemonDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+
+}
+
+    )
