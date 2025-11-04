c4dbb8ab1c2140056f2bb574d0193be9a1b8c151
Andrehlb <andreh.72@hotmail.com>
2025-11-04 17:06:55 -0300

    feat: Enable edge-to-edge display in DetailActivity
    Enables edge-to-edge display for `DetailActivity` to provide a more immersive user experience. This update leverages Data Binding for view initialization and handles system window insets to adjust padding appropriately.
    
    Key changes:
    - Calls `enableEdgeToEdge()` in `onCreate`.
    - Switches from `ActivityDetailBinding.inflate` to `DataBindingUtil.setContentView` for layout inflation.
    - Adds an `OnApplyWindowInsetsListener` to the root view to dynamically apply padding based on system bars.
    - Adds a `<data>` block to `activity_detail.xml` in preparation for future ViewModel integration.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/DetailActivity.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/DetailActivity.kt
@@ -1,7 +1,10 @@
 package br.com.venturus.andrehlb.minipokedex
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import br.com.venturus.andrehlb.minipokedex.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -9,7 +12,15 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        enableEdgeToEdge()
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+        binding.lifecycleOwner = this
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
