069d27ba9affd716e19f554d21b94974a6b745a1
Andrehlb <andreh.72@hotmail.com>
2025-11-04 19:36:51 -0300

    feat: Add Splash Screen and organize dependencies
    Adds the `core-splashscreen` dependency to implement a splash screen for the application.
    
    This commit also reorganizes the `dependencies` block in `app/build.gradle.kts` by grouping related libraries with comments for better readability. No dependency versions were changed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,19 +48,25 @@ android {
 }
 
 dependencies {
-
-    implementation ("androidx.core:core-ktx:1.13.1")
+    // Jetpack
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Lifecycle
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+
+    // Activity
     implementation("androidx.activity:activity-ktx:1.9.2")
+
+    // DataBinding
     implementation("androidx.databinding:databinding-runtime:8.5.0")
-    implementation("io.coil-kt:coil:2.7.0")
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
@@ -69,12 +75,11 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
-    // Coil (imagens)
+    // Coil
     implementation("io.coil-kt:coil:2.7.0")
 
-    // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-
-    // DataBinding
-    implementation("androidx.databinding:databinding-runtime:8.5.0")
+    // Testes
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
