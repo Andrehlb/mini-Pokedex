ea4e13487b23bf691bf55393104a965d6f8bf469
Andrehlb <andreh.72@hotmail.com>
2025-11-06 10:10:53 -0300

    feat: Enhance PokeApiService for pagination and detail fetching
    Updates the `PokeApiService` interface to support pagination in the Pokémon list endpoint and to fetch specific Pokémon details.
    
    Key changes:
    - `getPokemonList` now includes an optional `offset` parameter for paginating results, along with a default `limit` of 20.
    - `getPokemonByName` is updated to return a `PokemonDetailResponse` instead of `PokemonResponse`, enabling the retrieval of detailed data for a single Pokémon.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/PokeApiService.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/PokeApiService.kt
@@ -1,14 +1,15 @@
 package br.com.venturus.andrehlb.minipokedex.network
 
 import br.com.venturus.andrehlb.minipokedex.model.PokemonResponse
+import br.com.venturus.andrehlb.minipokedex.model.PokemonDetailResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PokeApiService {
     @GET("pokemon")
-    suspend fun getPokemonList(@Query("limit") limit: Int): PokemonResponse
+    suspend fun getPokemonList(@Query("limit") limit: Int = 20, @Query("offset") offset: Int = 0): PokemonResponse
 
     @GET("pokemon/{name}")
-    suspend fun getPokemonByName(@Path("name") name: String): PokemonResponse
+    suspend fun getPokemonByName(@Path("name") name: String): PokemonDetailResponse
 }
