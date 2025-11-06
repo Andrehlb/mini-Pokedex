725b8a17977de53702eb0cf5517d2aa3db9b8c02
Andrehlb <andreh.72@hotmail.com>
2025-11-06 08:42:21 -0300

    feat: Add endpoint to fetch Pokemon by name
    Adds a new `getPokemonByName` suspend function to the `PokeApiService` interface. This function allows fetching a single Pokemon's data by its name using a `GET` request to the `pokemon/{name}` endpoint.
    
    Additionally, the `getPokemonList` function signature has been updated to remove the `offset` parameter and its default value.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/PokeApiService.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/PokeApiService.kt
@@ -2,14 +2,13 @@ package br.com.venturus.andrehlb.minipokedex.network
 
 import br.com.venturus.andrehlb.minipokedex.model.PokemonResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
-
-
 
 interface PokeApiService {
     @GET("pokemon")
-    suspend fun getPokemonList(
-        @Query("limit") limit: Int = 20,
-        @Query("offset") offset: Int = 0
-    ): PokemonResponse
+    suspend fun getPokemonList(@Query("limit") limit: Int): PokemonResponse
+
+    @GET("pokemon/{name}")
+    suspend fun getPokemonByName(@Path("name") name: String): PokemonResponse
 }
