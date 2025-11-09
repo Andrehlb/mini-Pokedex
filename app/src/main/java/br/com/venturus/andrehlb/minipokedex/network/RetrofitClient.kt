302d0677967a252ffb0a5e032a16cd403496b40f
Andrehlb <andreh.72@hotmail.com>
2025-11-09 05:19:14 -0300

    refactor: Remove lazy initialization in RetrofitClient
    Removes the `lazy` delegates for initializing the `retrofit` and `pokeApiService` properties in the `RetrofitClient` object.
    
    This change simplifies the code by using direct initialization, as the properties are needed as soon as the object is accessed.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/RetrofitClient.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/RetrofitClient.kt
@@ -4,17 +4,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    val api: PokeApiService by lazy {
-        retrofit.create(PokeApiService::class.java)
-    }
+    val pokeApiService: PokeApiService = retrofit.create(PokeApiService::class.java)
 }
