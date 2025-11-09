ea1545df78a3c8b70189615dfbc129706ce8250e
Andrehlb <andreh.72@hotmail.com>
2025-11-08 22:01:54 -0300

    refactor: Separate Retrofit instance creation from service creation
    Extracts the `Retrofit` builder logic into a private `retrofit` property within the `RetrofitClient` object. The `PokeApiService` is now created from this `retrofit` instance.
    
    This change improves code organization by separating the `Retrofit` instance creation from the API service creation, while also renaming the public service property from `pokeApiService` to `api` for conciseness.

--- app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/RetrofitClient.kt
+++ app/src/main/java/br/com/venturus/andrehlb/minipokedex/network/RetrofitClient.kt
@@ -4,13 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
+
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
-    val pokeApiService: PokeApiService by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(PokeApiService::class.java)
+    }
+
+    val api: PokeApiService by lazy {
+        retrofit.create(PokeApiService::class.java)
     }
 }
